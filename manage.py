f7f803fc3eda90ddf285eda16024b9b2b955452a
FujiwaraKoki <ra0039ip@gmail.com>
2022-12-14 13:27:22 +0000

    Heroku settings.

---
+++ manage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env 
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+#sys.path.append("/foreigner_circle")
+
+
+#from foreigner_circle import views
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'community.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
